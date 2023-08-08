ec27cf918fc892712229e3649eb04dc0e931c81c
brashian <bsandovala12@gmail.com>
2023-08-08 10:42:33 -0300

    Creamos el apartado de login y register

--- src/app/models/messi.ts
+++ src/app/models/messi.ts
@@ -1,7 +1,7 @@
-export interface TarjetaInicio {
+export interface Messi {
+    id: number;
     titulo: string;
     descripcion: string;
     image: string;
     alt: string;
-    
 }
--- src/app/modules/inicio/pages/inicio/inicio.component.ts
+++ src/app/modules/inicio/pages/inicio/inicio.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { TarjetaInicio } from 'src/app/models/modelos';
+import { Messi } from 'src/app/models/messi';
 
 @Component({
   selector: 'app-inicio',
@@ -8,15 +8,31 @@ import { TarjetaInicio } from 'src/app/models/modelos';
 })
 export class InicioComponent {
   // PROPIEDAD PUBLICA (TIPO ARRAY)
-  public info: TarjetaInicio[];
+  public info: Messi[];
+
 
   constructor(){
     this.info = [
       {
+        id: 1,
         titulo: "Goku",
         descripcion: "Goku fase dios",
         image: "https://i.blogs.es/023c0d/goku-super-saiyajin-dios-akira-toriayma-4/1366_2000.jpeg",
         alt: "kame kame kame haaaaaaaaaaaa"
+      },
+      {
+        id: 2,
+        titulo: "Messi",
+        descripcion: "El mejor jugador del mundo",
+        image: "https://media.tycsports.com/files/2023/02/10/532928/lionel-messi_1440x810_wmk.webp",
+        alt: "messi"
+      },
+      {
+        id: 3,
+        titulo: "Julian Alvarez",
+        descripcion: "Otro mejor jugador del mundo",
+        image: "https://tmssl.akamaized.net/images/foto/galerie/alvarez-julian-2022-23-argentina-1669219939-97261.jpg?lm=1669219952",
+        alt: "hola"
       }
     ]
   }
